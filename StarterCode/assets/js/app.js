b2ff7dd25dbe6b48fce728edd3031e3cce1a0df2
sigkarmoil <haezers@gmail.com>
2020-09-09 22:20:16 -0400

    finished

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -4,11 +4,12 @@ d3.csv("assets/data/data.csv").then(function(data) {
     // console.log(data);
 
     // log a list of names
-    var poverty = data.map(d => d.poverty);
+    var poverty = data.map(d => parseInt(d.poverty));
+    // var poverty = data.map(d => d.poverty);
  
     // console.log("poverty", poverty);
 
-    var obesity = data.map(d => d.obesity);
+    var obesity = data.map(d =>parseInt( d.obesity));
 
     // console.log("obesity", obesity);
 
@@ -17,7 +18,7 @@ d3.csv("assets/data/data.csv").then(function(data) {
     console.log("obesity", state);
 
     // svg container
-    var svgHeight = 400;
+    var svgHeight = 600;
     var svgWidth = 1000;
 
     // margins
@@ -52,12 +53,14 @@ d3.csv("assets/data/data.csv").then(function(data) {
     //     .range([0, chartWidth])
     //     .padding(1);
 
+    console.log(d3.min(poverty))
+
     // scale x to chart width
     var xScale = d3.scaleLinear()
-        .domain(poverty)
-        // .domain([d3.min(poverty), d3.max(poverty)])
-        // .domain(0,100)
-        .range([ chartWidth, 0])
+
+        .domain([d3.min(poverty) - d3.min(poverty)/10, d3.max(poverty)])
+
+        .range([0, chartWidth ])
 
 
     // scale y to chart height
@@ -72,8 +75,7 @@ d3.csv("assets/data/data.csv").then(function(data) {
     // set x to the bottom of the chart
     chartGroup.append("g")
     .attr("transform", `translate(0, ${chartHeight})`)
-    .call(xAxis
-        .ticks(10));
+    .call(xAxis);
 
     
 
@@ -97,7 +99,7 @@ d3.csv("assets/data/data.csv").then(function(data) {
     .attr("stroke","white")
     .attr("stroke-width", "2")
 
-    // Append text to chartGroup
+    // Append text to bubbles
     chartGroup.selectAll(".state")
     .data(obesity)
     .enter()
@@ -108,6 +110,24 @@ d3.csv("assets/data/data.csv").then(function(data) {
     .text( (d,i) => state[i] )
     .attr("font-size", 8)
     .attr("fill","white")
+
+    // Append text to X axis
+    svg.append("text")             
+    .attr("transform",
+          "translate(" + (chartWidth/2 + margin.left) + " ," + 
+                         (chartHeight + margin.top + 40) + ")")
+    .style("text-anchor", "middle")
+    .text("Poverty")
+    .attr("font-size", 20); 
+
+      // text label for the y axis
+    svg.append("text")
+    .attr("transform", "translate(0," + (chartHeight / 2) + ") rotate(-90)")
+
+    .attr("dy", "1em")
+    .style("text-anchor", "middle")
+    .text("Obesity")
+    .attr("font-size", 20); 
 
 
   }).catch(function(error) {
